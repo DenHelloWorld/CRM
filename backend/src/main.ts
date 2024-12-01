8b15099aa0771944151ac54bdf14902c586d33dc
Denis Saveliev <96991873+DenHelloWorld@users.noreply.github.com>
2024-12-01 02:27:54 +0200

    PostrdeSQL DB in Docker (#7)
    * chore: add basic setup prisma with postgresql in docker
    
    * docs: create empty package for future full app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const PORT = process.env.API_PORT;
+  try {
+    await app.listen(PORT);
+    console.info(`App listening on port ${PORT}`);
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 bootstrap();
