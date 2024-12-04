cdd55b9ef9a9d3ceebe6aa99da531f60df896ad9
Denis Saveliev <denissaveliev23@gmail.com>
2024-12-04 21:18:17 +0200

    feat: add is active styling in menu

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,6 +7,18 @@ export const routes: Routes = [
       import('./features/customer-dashboard/customer-dashboard.component').then(
         (x) => x.CustomerDashboardComponent,
       ),
-    data: { headerBreadcrumb: 'Customer Dashboard' },
+    data: { headerBreadcrumb: 'Dashboard' },
+  },
+  {
+    path: 'users',
+    loadComponent: () =>
+      import('./features/users/users.component').then((x) => x.UsersComponent),
+    data: { headerBreadcrumb: 'Users' },
+  },
+  {
+    path: 'tasks',
+    loadComponent: () =>
+      import('./features/tasks/tasks.component').then((x) => x.TasksComponent),
+    data: { headerBreadcrumb: 'Tasks' },
   },
 ];
---
+++ frontend/src/app/features/tasks/tasks.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tasks',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      tasks works!
+    </p>
+  `,
+  styles: ``
+})
+export class TasksComponent {
+
+}
